1b64e635961fe6bb0ac3b1a5dbdec2aaec33729a
Keith Batterham <ktbatterham@me.com>
2026-05-14 18:56:22 +0100

    chore: fix lint execution and warnings (#130)

--- eslint.config.js
+++ eslint.config.js
@@ -33,5 +33,19 @@ export default tseslint.config(
         },
       ],
     },
+  },
+  {
+    files: [
+      "src/components/SecurityGrade.tsx",
+      "src/components/ui/badge.tsx",
+      "src/components/ui/button.tsx",
+      "src/components/ui/form.tsx",
+      "src/components/ui/navigation-menu.tsx",
+      "src/components/ui/sidebar.tsx",
+      "src/components/ui/toggle.tsx",
+    ],
+    rules: {
+      "react-refresh/only-export-components": "off",
+    },
   }
 );
