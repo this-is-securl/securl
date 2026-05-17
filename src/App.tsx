21bde28103302eea6e4e00b04e5a4e12c82aa13c
ktbatterham <ktbatterham@me.com>
2026-05-17 18:27:41 +0100

    feat: add telemetry dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -3,25 +3,37 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { useEffect } from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 import Index from "./pages/Index";
+import Telemetry from "./pages/Telemetry";
 import { recordPageLoad } from "./lib/apiClient";
 
 const queryClient = new QueryClient();
 
-const App = () => {
-  useEffect(() => {
-    recordPageLoad();
-  }, []);
+const PageLoadRecorder = () => {
+  const location = useLocation();
 
+  useEffect(() => {
+    if (location.pathname === "/telemetry") {
+      return;
+    }
+    recordPageLoad();
+  }, [location.pathname]);
+
+  return null;
+};
+
+const App = () => {
   return (
     <QueryClientProvider client={queryClient}>
       <TooltipProvider>
         <Toaster />
         <Sonner />
         <BrowserRouter>
+          <PageLoadRecorder />
           <Routes>
             <Route path="/" element={<Index />} />
+            <Route path="/telemetry" element={<Telemetry />} />
           </Routes>
         </BrowserRouter>
       </TooltipProvider>
