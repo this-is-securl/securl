284a0617fd324d39667c8543a8d8ba43c7b8ca3a
ktbatterham <ktbatterham@me.com>
2026-05-17 18:32:12 +0100

    refactor: make telemetry dashboard local-only

--- src/App.tsx
+++ src/App.tsx
@@ -3,37 +3,25 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { useEffect } from "react";
-import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
-import Telemetry from "./pages/Telemetry";
 import { recordPageLoad } from "./lib/apiClient";
 
 const queryClient = new QueryClient();
 
-const PageLoadRecorder = () => {
-  const location = useLocation();
-
-  useEffect(() => {
-    if (location.pathname === "/telemetry") {
-      return;
-    }
-    recordPageLoad();
-  }, [location.pathname]);
-
-  return null;
-};
-
 const App = () => {
+  useEffect(() => {
+    recordPageLoad();
+  }, []);
+
   return (
     <QueryClientProvider client={queryClient}>
       <TooltipProvider>
         <Toaster />
         <Sonner />
         <BrowserRouter>
-          <PageLoadRecorder />
           <Routes>
             <Route path="/" element={<Index />} />
-            <Route path="/telemetry" element={<Telemetry />} />
           </Routes>
         </BrowserRouter>
       </TooltipProvider>
